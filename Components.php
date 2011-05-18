9c8c0a0e7e71337853fea47583ee342bb48dd4ac
dsjellz <davidjellesma@gmail.com>
2011-05-18 11:47:09 -0700

    Modifying Components.php CTCTException to not use the optional 3rd argument, to maintain backwards compatibility with versions of PHP prior to 5.3.0

--- Components.php
+++ Components.php
@@ -1431,7 +1431,7 @@ class Utility{
 
 class CTCTException extends Exception{
     public function __construct($message, $code = 0, Exception $previous = null){
-        parent::__construct($message, $code, $previous);
+        parent::__construct($message, $code);
     }
 
     public function generateError($msgPrefix=null){
