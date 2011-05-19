bdbfa7be8ef85cf74ff3020b76dbc2b068560901
dsjellz <davidjellesma@gmail.com>
2011-05-19 08:39:09 -0700

    Force Authentication.php to log an error should an http request fail for any reason, as opposed to just displaying it on the screen.

--- Authentication.php
+++ Authentication.php
@@ -1070,7 +1070,7 @@ class CTCTRequest{
                 $return = $return;
             }
         } catch(CTCTException $e) {
-            echo $e->getMessage();
+            echo $e->generateError();
         }
         return $return;
     }
