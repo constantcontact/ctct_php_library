fc9d65ac5ef8817de4fb840083ff2e9658678b69
Constant Contact Support <APISupport@constantcontact.com>
2012-06-26 14:28:12 -0600

    Update master

--- index.php
+++ index.php
@@ -43,12 +43,14 @@
 		?>
 	<html>
 	<body>
-		<h1>oAuth2</h1>
+		<h1>OAuth 2.0 Example</h1>
 	
-	
-		<h3>You need to authenticate</h3>
+		<h3>Click the link below to authorize your application to access your Constant Contact account resources through this PHP Wrapper Library, and obtain an access token that will be used to authenticate other API Requests made within the current session. The Wrapper Library will store the Authentication data in the PHP $_SESSION array, by default.</h3>
+		
+		<h3>Developers should store the authentication data securely on their server and include code in application scripts to restore this to the $_SESSION array when the base application script is executed, to avoid the need to re-authenticate when applications scripts are executed at the start of each new session.</h3>
+		
 		<?php
-		// you must encode your redirect URL and the link contains the varaibles from config.php
+		// you must encode your redirect URL and the link contains the variables from config.php
 		$theRequest = urlencode ( $verificationURL );
 		echo "<a href='https://oauth2.constantcontact.com/oauth2/oauth/siteowner/authorize?response_type=code&client_id=" 
 		. $apikey . "&redirect_uri=" 
